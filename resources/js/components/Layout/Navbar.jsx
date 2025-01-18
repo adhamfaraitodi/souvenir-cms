348fe807da0ff3dd5b08191fa98807cc4e152098
rianwpzx <rianwp2015@gmail.com>
2025-01-18 16:42:30 +0700

    Fix: add navigation route to sidebar

--- resources/js/components/Layout/Navbar.jsx
+++ resources/js/components/Layout/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="bg-lightBlue shadow">
             <div className="flex flex-row justify-between items-center p-2 px-5 gap-2">
                 <div className="font-bold text-sm sm:text-xl">Hi, Admin</div>
-                <ProfileMenu />
+                {/* <ProfileMenu /> */}
             </div>
         </nav>
     );
